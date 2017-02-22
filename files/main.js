a838c36f40070fec36115e1a86ea8380067c61e0
kshart <kshart@yandex.ru>
2017-02-22 23:48:06 +0500

    Update Signed-off-by:kshart <kshart@yandex.ru>

--- files/main.js
+++ files/main.js
@@ -9,59 +9,59 @@
  *    Артем - initial API and implementation and/or initial documentation
  */
 $.page.create([
-				{url:"/", options:{
-					urlEqual:false,
-					title:"Титл",
-					onshow:function(e) {
-						console.log(this.title);
-						$("#content").html("Главная страница.");
-					}
-				}},
-				{url:"/catalog", options:{
-					title:"Титлgg",
-					onshow:function(e) {
-						console.log(this.title);
-						$("#content").html("Каталог.<div id='catalog_id'></div><p id='catalog_preloader_id'></p>");
-					},
-					onupdate:function(e) {
-						$("#catalog_preloader_id").html("load"+e.url);
-					}
-				}},
-				{url:"/catalog/e", options:{
-					urlEqual:false,
-					title:"Категория е",
-					parent:"/catalog",
-					onshow:function(e) {
-						console.log(this.title);
-						$("#catalog_id").html("Категория е"+e.options.overURL+".<div id='catalog_e_id'></div>");
-					},
-					onupdate:function(e) {
-						$("#catalog_id").text("Категория е"+e.options.overURL+".");
-					},
-					onhide:function(e) {
-						$("#catalog_id").html("");
-					}
-				}},
-				{url:"/catalog/e/moi", options:{
-					title:"moi",
-					parent:"/catalog/e",
-					onshow:function(e) {
-						console.log(this.title);
-						$("#catalog_e_id").html("moi");
-					},
-					onhide:function(e) {
-						console.log("hide"+e.url);
-					}
-				}},
-				{url:"/catalog/e/tvoi", options:{
-					title:"tvoi",
-					parent:"/catalog/e",
-					onshow:function(e) {
-						$("#catalog_e_id").html("tvoi");
-					},
-					onhide:function(e) {
-						console.log("hide"+e.url);
-					}
-				}}
-			]);
-			$.page.init();
+	{url:"/", options:{
+		urlEqual:false,
+		title:"Титл",
+		onshow:function(e) {
+			console.log(this.title);
+			$("#content").html("Главная страница.");
+		}
+	}},
+	{url:"/catalog", options:{
+		title:"Титлgg",
+		onshow:function(e) {
+			console.log(this.title);
+			$("#content").html("Каталог.<div id='catalog_id'></div><p id='catalog_preloader_id'></p>");
+		},
+		onupdate:function(e) {
+			$("#catalog_preloader_id").html("load"+e.url);
+		}
+	}},
+	{url:"/catalog/e", options:{
+		urlEqual:false,
+		title:"Категория е",
+		parent:"/catalog",
+		onshow:function(e) {
+			console.log(this.title);
+			$("#catalog_id").html("Категория е"+e.options.overURL+".<div id='catalog_e_id'></div>");
+		},
+		onupdate:function(e) {
+			$("#catalog_id").text("Категория е"+e.options.overURL+".");
+		},
+		onhide:function(e) {
+			$("#catalog_id").html("");
+		}
+	}},
+	{url:"/catalog/e/moi", options:{
+		title:"moi",
+		parent:"/catalog/e",
+		onshow:function(e) {
+			console.log(this.title);
+			$("#catalog_e_id").html("moi");
+		},
+		onhide:function(e) {
+			console.log("hide"+e.url);
+		}
+	}},
+	{url:"/catalog/e/tvoi", options:{
+		title:"tvoi",
+		parent:"/catalog/e",
+		onshow:function(e) {
+			$("#catalog_e_id").html("tvoi");
+		},
+		onhide:function(e) {
+			console.log("hide"+e.url);
+		}
+	}}
+]);
+$.page.init();
